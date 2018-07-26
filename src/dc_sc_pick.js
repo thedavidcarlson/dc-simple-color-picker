8052d60164db8718efbf375bd68bfe8dc903cdb8
unknown <davidcarlson2014@gmail.com>
2018-07-25 21:21:12 -0700

    Added a singular initialization function and added a hexToRgb function

--- src/dc_sc_pick.js
+++ src/dc_sc_pick.js
@@ -2,9 +2,30 @@ class SimpleColorPicker {
     constructor( color ) {
         this.color = color;
     }
+    hexToRgb( hex ) {
+        // Expand shorthand form (e.g. "03F") to full form (e.g. "0033FF")
+        var shorthandRegex = /^#?([a-f\d])([a-f\d])([a-f\d])$/i;
+
+        hex = hex.replace( shorthandRegex, function(m, r, g, b) {
+            return r + r + g + g + b + b;
+        } );
+
+        var result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec( hex );
+        return result ? {
+            r: parseInt( result[ 1 ], 16 ),
+            g: parseInt( result[ 2 ], 16 ),
+            b: parseInt( result[ 3 ], 16 )
+        } : null;
+    }
 }
 
 const initScPicker = function() {
+    let scInput = document.querySelector( '.sc-picker' );
+
+    window.scPicker = new SimpleColorPicker( scInput.value );
+}
+
+const initScPickers = function() {
     window.scPickers = [];
 
     let scInputs = document.querySelectorAll( '.sc-picker' );
@@ -16,8 +37,5 @@ const initScPicker = function() {
         scInput.setAttribute( 'data-sc-picker-id', i );
         window.scPickers.push( new SimpleColorPicker( scInput.value ) );
 
-        console.log( 'Simple Color Picker #' + ( i + 1 ) + ': ' + window.scPickers[ i ].color );
     }
 }
-
-initScPicker();
